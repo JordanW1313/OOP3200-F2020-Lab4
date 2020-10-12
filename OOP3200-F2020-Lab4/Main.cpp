771359ca4ed7b569b4b1b9a52e2da1ce717aebad
ahmedrizvi <ahmed.riz117@gmail.com>
2020-10-12 17:58:31 -0400

    Fixed DeckToString method and added some comments

--- OOP3200-F2020-Lab4/Main.cpp
+++ OOP3200-F2020-Lab4/Main.cpp
@@ -1,3 +1,15 @@
+/****************************************************************
+* Name: Jordan Wriker & Ahmed Rizvi
+* Date: 11th October, 2020
+* Course: OOP-3200
+* File: OOP3200-F2020-Lab4
+* Purpose: The purpose of this lab is to create a collection class
+*		   that contains PlayingCard objects. In this case, the new
+*		   class contains 52 PlayingCard, as seen from a typical deck
+*		   of playing cards.
+*
+******************************************************************/
+
 #include <ctime>
 #include <iostream>
 #include "StandardDeck.h"
@@ -13,7 +25,7 @@ int main()
 	//srand(time(nullptr)); // seed the random number generator
 
 
-	// create a deck
+	// Create a deck for testing.
 	StandardDeck test_deck1;
 
 	// Show the deck object as initialized
@@ -22,7 +34,7 @@ int main()
 	Pause();
 
 	// Draw the next card and show it
-	test_deck1.DrawNextCard();
+	//test_deck1.DrawNextCard();
 	test_deck1.DrawNextCard();
 	test_deck1.DrawNextCard();
 
@@ -40,14 +52,22 @@ int main()
 	// Shuffle the deck and show the result
 	test_deck1.Shuffle();
 	test_deck1.DeckToString();
+	std::cout << std::endl << std::endl;
 
+	Pause();
+	
 	// Reset the deck and show the result
 	test_deck1.Initialize();
 	test_deck1.DeckToString();
+	std::cout << std::endl << std::endl;
 
-	// Show assignment operator working
-	StandardDeck test_deck2;
-	test_deck2 = test_deck1;
+	Pause();
+	
+	// Show copy constructor and = overload.
+	test_deck1.Shuffle();
+	StandardDeck test_deck2 = test_deck1;
+	StandardDeck test_deck3;
+	test_deck2 = test_deck3;
 
 	test_deck2.DeckToString();
 }
--- OOP3200-F2020-Lab4/StandardDeck.cpp
+++ OOP3200-F2020-Lab4/StandardDeck.cpp
@@ -22,6 +22,7 @@ StandardDeck::StandardDeck(const StandardDeck& source)
 {
 	SetVector(source.myCardsVector[0]);
 	size = source.size;
+	std::cout << "\nCopy constructor was called.\n";
 }
 
 // Assignment operator overload.
@@ -29,7 +30,8 @@ StandardDeck& StandardDeck::operator=(StandardDeck& source)
 {
 	this->SetVector(source.myCardsVector[0]);
 	this->size = source.size;
-
+	std::cout << "\nAssignment operator was used.\n";
+	
 	return (*this);
 }
 
@@ -46,62 +48,33 @@ void StandardDeck::CardsRemaining() const
 	std::cout << "The deck currently has " << currentSize << " cards remaining." << std::endl;
 }
 
+// Prints out the PlayingCard objects in the vector.
 void StandardDeck::DeckToString()
 {
 	int deckSize = myCardsVector[0].size();
 
 	std::cout << "Deck Size: " <<  deckSize << std::endl;
-	std::cout << "==================================================" << std::endl;
+	std::cout << "==================================================\n" << std::endl;
 
 	for (int i = 0; i < deckSize;)
 	{
-		/*int column = 0;
-
-		if (column > 3)
+		int loopCheck = i + 4;
+		if ((loopCheck) > deckSize)
 		{
-			std::cout << std::left << std::setw(8) << std::setfill('-') << myCardsVector[0][i].GetRank() << " " << myCardsVector[0][i].GetSuit() << "(" << i << ")" << "	" << std::endl;
-			column = 0;
+			std::cout << std::left << std::setw(8) << myCardsVector[0][i].GetRank() << " " << myCardsVector[0][i].GetSuit() << "(" << i + 1 << ")" << "	";
+			i++;
 		}
 		else
 		{
-			std::cout << std::left << std::setw(8) << std::setfill('-') << myCardsVector[0][i + 1].GetRank() << " " << myCardsVector[0][i + 1].GetSuit() << "(" << i + 1 << ")" << "	";
+			std::cout << std::left << std::setw(8) << std::setfill('-') << myCardsVector[0][i].GetRank() << " " << myCardsVector[0][i].GetSuit() << "(" << i << ")" << "	"
+				<< std::left << std::setw(8) << myCardsVector[0][i + 1].GetRank() << " " << myCardsVector[0][i + 1].GetSuit() << "(" << i + 1 << ")" << "	"
+				<< std::left << std::setw(8) << myCardsVector[0][i + 2].GetRank() << " " << myCardsVector[0][i + 2].GetSuit() << "(" << i + 2 << ")" << "	"
+				<< std::left << std::setw(8) << myCardsVector[0][i + 3].GetRank() << " " << myCardsVector[0][i + 3].GetSuit() << "(" << i + 3 << ")" << "	" << std::endl;
+			i += 4;
 		}
-		if (column > 3)
-		{
-			std::cout << std::left << std::setw(8) << std::setfill('-') << myCardsVector[0][i + 1].GetRank() << " " << myCardsVector[0][i + 1].GetSuit() << "(" << i + 1 << ")" << "	" << std::endl;
-			column = 0;
-		}
-		else
-		{
-			std::cout << std::left << std::setw(8) << std::setfill('-') << myCardsVector[0][i + 1].GetRank() << " " << myCardsVector[0][i + 1].GetSuit() << "(" << i + 1<< ")" << "	";
-		}
-		if (column > 3)
-		{
-			std::cout << std::left << std::setw(8) << std::setfill('-') << myCardsVector[0][i + 2].GetRank() << " " << myCardsVector[0][i + 2].GetSuit() << "(" << i + 2 << ")" << "	" << std::endl;
-			column = 0;
-		}
-		else
-		{
-			std::cout << std::left << std::setw(8) << std::setfill('-') << myCardsVector[0][i + 2].GetRank() << " " << myCardsVector[0][i + 2].GetSuit() << "(" << i + 2 << ")" << "	";
-		}
-		if (column > 3)
-		{
-			std::cout << std::left << std::setw(8) << std::setfill('-') << myCardsVector[0][i + 3].GetRank() << " " << myCardsVector[0][i + 3].GetSuit() << "(" << i + 3 << ")" << "	" << std::endl;
-			column = 0;
-		}
-		else
-		{
-			std::cout << std::left << std::setw(8) << std::setfill('-') << myCardsVector[0][i + 3].GetRank() << " " << myCardsVector[0][i + 3].GetSuit() << "(" << i + 3 << ")" << "	";
-		}*/
-
-		std::cout << std::left << std::setw(8) << std::setfill('-') << myCardsVector[0][i].GetRank() << " " << myCardsVector[0][i].GetSuit() << "(" << i << ")" << "	"
-			<< std::left << std::setw(8) << myCardsVector[0][i + 1].GetRank() << " " << myCardsVector[0][i + 1].GetSuit() << "(" << i + 1 << ")" << "	"
-			<< std::left << std::setw(8) << myCardsVector[0][i + 2].GetRank() << " " << myCardsVector[0][i + 2].GetSuit() << "(" << i + 2 << ")" << "	"
-			<< std::left << std::setw(8) << myCardsVector[0][i + 3].GetRank() << " " << myCardsVector[0][i + 3].GetSuit() << "(" << i + 3 << ")" << "	" << std::endl;
-		i += 4;
 	}
 
-	std::cout << "==================================================" << std::endl;
+	std::cout << "\n==================================================" << std::endl;
 
 }
 
@@ -124,7 +97,6 @@ void StandardDeck::Initialize()
 			PlayingCard currentCard; // Card currently being built.
 			currentCard.SetRank(PlayingCard::CARD_RANK[rank]);
 			currentCard.SetSuit(PlayingCard::CARD_SUIT[suit]);
-			//std::cout << currentCard.GetRank() << " " << currentCard.GetSuit() << std::endl;
 			myCardsVector->push_back(currentCard); // Allocate the new card into the vector.
 		}
 	}
@@ -140,10 +112,6 @@ void StandardDeck::DrawNextCard()
 	assert(!myCardsVector[0].empty());
 	myCardsVector[0].erase(myCardsVector[0].begin());
 	size = currentSize - 1;
-	/*assert(!myCardsVector[0].empty());
-	myCardsVector[0].erase(myCardsVector[0].begin());
-	myCardsVector[0].resize(currentSize - 1);
-	size = currentSize - 1;*/
 }
 
 // Removes a random object from the array, and shifts the indices of the following objects by 1 to the left.
@@ -156,11 +124,7 @@ void StandardDeck::DrawRandomCard()
 	std::cout << "The random card is the " << myCardsVector[0][randNum].GetRank() << " of " << myCardsVector[0][randNum].GetSuit() << "." << std::endl;
 
 	myCardsVector[0].erase(myCardsVector[0].begin()+randNum); // Removes the element at random index.
-	/*assert(!myCardsVector[0].empty());
-	myCardsVector[0][randNum] = std::move(myCardsVector[0].front());
-	myCardsVector[0].erase(myCardsVector[0].begin());
-	myCardsVector[0].resize(currentSize - 1);
-	size = currentSize - 1;*/
+	size = currentSize - 1;
 }
 
 void StandardDeck::Shuffle()
